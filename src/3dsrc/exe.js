6bbc2a8dd8b8ddd4b4f887c4419ca6f04171805a
kkh061101 <kkh061101@naver.com>
2023-04-11 21:09:57 +0900

    Update exe.js

--- src/3dsrc/exe.js
+++ src/3dsrc/exe.js
@@ -1,15 +1,23 @@
-import { PresentationControls, Stage } from "@react-three/drei";
+import { Float, MeshReflectorMaterial, PresentationControls, Stage } from "@react-three/drei";
 import React from "react";
 
 function Exe() {
   return <>
-    <PresentationControls speed={1.5} global zoom={0.7} polar={[-0.1, Math.PI / 4]}>
-      <Stage environment={"city"} intensity={1} contactShadow={false} shadowBias={-0.0015}>
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
+    <PresentationControls speed={1.5} global zoom={0.7} polar={[-1, Math.PI / 4]}>
+      <Stage environment={"city"} intensity={0.6} shadows={{ type: 'false', bias: -0.0015 }}>
+        <Float speed={1} rotationIntensity={1} floatIntensity={1} floatingRange={[0.001, 0.01]}>
+          <mesh>
+            <boxGeometry args={[0.01, 0.01, 0.01]} />
+            <meshStandardMaterial />
+          </mesh>
+        </Float>
       </Stage>
+      <mesh position={[0, -0.005, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[170, 170]} />
+        <MeshReflectorMaterial blur={[300, 100]} resolution={2048}
+          mixBlur={1} mixStrength={40} roughness={1} depthScale={1.2} minDepthThreshold={0.4}
+          maxDepthThreshold={1.4} color={"#101010"} metalness={0.5} />
+      </mesh>
     </PresentationControls>
   </>;
 }
